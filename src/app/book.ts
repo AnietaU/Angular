a3b1e442d3c33c9f2ea19c702703d0b09a9110ea
Anitha U <anneu074@gmail.com>
2023-04-04 23:27:28 +0530

    created a sample HTML template

---
+++ src/app/book.ts
@@ -0,0 +1,10 @@
+export class Book {
+    constructor(
+       public  bookName:string,
+        public author:string,
+        public genre:string,
+        public cost:number,
+        public isbn:number
+
+    ){}
+}
